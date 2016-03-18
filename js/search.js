d0f59fad4786ee964ee3fb816426243aa6855bfc
Brad London <london.brad@gmail.com>
2016-03-18 10:44:22 -0700

    list repos on the page

--- js/search.js
+++ js/search.js
@@ -1,8 +1,14 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(userName){
-  $.get('https://api.github.com/users/' + userName +'?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + userName +'/repos?access_token=' + apiKey).then(function(response){
     console.log(JSON.stringify(response));
+    $('#results').append('<h3>Public Repositories</h3><ul>');
+    response.forEach(function(repo){
+    $('#results').append('<li>' + repo.name + '</li>');
+    });
+    $('#results').append('</ul>');
+
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
