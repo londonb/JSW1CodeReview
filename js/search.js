c390b0de59ea78175183e922eeca231e3f74ce13
Brad London <london.brad@gmail.com>
2016-03-18 11:38:04 -0700

    fill in blank descriptions

--- js/search.js
+++ js/search.js
@@ -2,14 +2,21 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(userName){
   $.get('https://api.github.com/users/' + userName +'/repos?per_page=100&access_token=' + apiKey).then(function(response){
+
     console.log(JSON.stringify(response));
+
     $('#results').append('<h3>Public Repositories</h3><ul>');
     response.forEach(function(repo){
-    $('#results').append('<li>' + repo.name + ' : ' + repo.description + '</li>');
-    });
-    $('#results').append('</ul>');
+
+      if(repo.description === ""){
+        $('#results').append('<li>' + repo.name + ' : FILL OUT YOUR DESCRIPTIONS!!!</li>');
+      } else {
+        $('#results').append('<li>' + repo.name + ' : ' + repo.description + '</li>');
+         }
+         $('#results').append('</ul>');
 
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
+});
 };
