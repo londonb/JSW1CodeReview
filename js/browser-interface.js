b1bf93a7ae6164e757de1bce1be8097a7df152c7
Brad London <london.brad@gmail.com>
2016-03-18 12:06:00 -0700

    add stylings

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,6 +1,7 @@
 var getRepos = require('./../js/search.js').getRepos;
 
 $(document).ready(function(){
+    $('#results').empty();
   $('#userRepo').click(function(){
    var userName = $('#userName').val();
    getRepos(userName);
